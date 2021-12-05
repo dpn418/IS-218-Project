70dd7daab8e9d65c5231297c2c2fdc407638e0cc
dpn418 <60945105+dpn418@users.noreply.github.com>
2021-12-05 13:12:58 -0500

    Update is218.sql

--- is218.sql
+++ is218.sql
@@ -8,24 +8,26 @@ DROP TABLE IF EXISTS `toDo`;
 
 -- create the tables
 CREATE TABLE users (
-                          username    VARCHAR (250)   NOT NULL UNIQUE,
-                          fname       VARCHAR (60)   NOT NULL,
-                          lname       VARCHAR(60)    NOT NULL,
-                          email       VARCHAR(250)    NOT NULL UNIQUE,
-                          password    VARCHAR(30)    NOT NULL,
-                          PRIMARY KEY (username),
-                          CONSTRAINT chk_email CHECK(email LIKE '%_@___%')
+	username    VARCHAR (250)  NOT NULL UNIQUE,
+	fname       VARCHAR (60)   NOT NULL,
+	lname       VARCHAR(60)    NOT NULL,
+	email       VARCHAR(250)   NOT NULL UNIQUE,
+	password    VARCHAR(30)    NOT NULL,
+	prevpass1 	VARCHAR(30)    NOT NULL,--Previous password after 1 password change
+	prevpass2 	VARCHAR(30)    NOT NULL,--Previous password after 2 password changes
+	PRIMARY KEY (username),
+	CONSTRAINT chk_email CHECK(email LIKE '%_@___%')
 
 );
 
 CREATE TABLE toDo (
-                            taskId        INT            DEFAULT NULL AUTO_INCREMENT,
-                            username      VARCHAR(60)    NOT NULL,
-                            dueDate       DATETIME       NOT NULL,     -- check this later
-                            currentDate   DATETIME     default  current_timestamp,
-                            description   VARCHAR(250)   NOT NULL,
-                            urgency       TINYINT(2)    NOT NULL, -- TINYINT(2) since starts at 0 'Normal','Important','Very Important'
+	taskId        INT           DEFAULT NULL AUTO_INCREMENT,
+	username      VARCHAR(60)   NOT NULL,
+	dueDate       DATETIME      NOT NULL,     -- check this later
+	currentDate   DATETIME     	default  current_timestamp,
+	description   VARCHAR(250)  NOT NULL,
+	urgency       TINYINT(2)    NOT NULL, -- TINYINT(2) since starts at 0 'Normal','Important','Very Important'
 
-                            PRIMARY KEY (taskId),
-                            FOREIGN KEY (username) REFERENCES users(username)
+	PRIMARY KEY (taskId),
+	FOREIGN KEY (username) REFERENCES users(username)
 );
