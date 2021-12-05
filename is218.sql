2bacb8e19f0c0a3f3f3369b8a10ca74b0f7a54c7
dpn418 <60945105+dpn418@users.noreply.github.com>
2021-12-05 18:46:02 -0500

    changed primary key

--- is218.sql
+++ is218.sql
@@ -6,14 +6,14 @@ DROP TABLE IF EXISTS `users`;
 -- create the tables
 
 CREATE TABLE users (
+	email       	VARCHAR(250)   	NOT NULL UNIQUE,
 	username       	VARCHAR(250)    NOT NULL UNIQUE,
 	fname     	  	VARCHAR(60)   	NOT NULL,
 	lname     	  	VARCHAR(60)    	NOT NULL,
-	email       	VARCHAR(250)   	NOT NULL UNIQUE,
 	password    	VARCHAR(30)    	NOT NULL,
 	prevpass1 		VARCHAR(30)    	DEFAULT NULL,
 	prevpass2 		VARCHAR(30)    	DEFAULT NULL,
-	PRIMARY KEY (username),
+	PRIMARY KEY (email),
 	CONSTRAINT chk_email CHECK(email LIKE '%_@_%._%')
 );
 /* users table notes
@@ -22,14 +22,14 @@ prevpass2 -> 2 passwords ago
 */
 CREATE TABLE toDo (
 	taskID        	INT           	NOT NULL AUTO_INCREMENT,
-	username      	VARCHAR(250)	NOT NULL,
+	email      		VARCHAR(250)	NOT NULL,
 	completed		TINYINT(2)		NOT NULL DEFAULT 0,
 	dueDate       	DATETIME      	NOT NULL,     
 	title			VARCHAR(50)		NOT NULL,
 	description   	VARCHAR(250)  	NOT NULL,
 	urgency    		TINYINT(2)    	NOT NULL, 
 	PRIMARY KEY (taskID),
-	FOREIGN KEY (username) REFERENCES users(username)
+	FOREIGN KEY (email) REFERENCES users(email)
 );
 /* todo table notes
 we don't really need this but if we do its here: currentDate   DATETIME     	default  current_timestamp,
@@ -47,15 +47,15 @@ VALUES
 ('Zuccd!', 'Mark', 'Zuckerberg', 'givemey0urdata@gmail.com','N0FACESonlyMETA',NULL,NULL);
 
 
-INSERT INTO toDo (username, dueDate, title, description, urgency) 
+INSERT INTO toDo (email, dueDate, title, description, urgency) 
 VALUES
-('Sk8rboii', '2021-12-10 04:20:00', '!KICKFLIP TIME!', 'DO A KICKFLIP!', 0),
-('Sk8rboii', '2021-12-09 23:59:59', 'Spread the word', 'Tell everyone to DO A KICKFLIP!', 1), 
-('Ya.boy', '2021-12-10 04:20:00', '!KICKFLIP TIME!', 'DO A KICKFLIP!', 0),
-('Ya.boy', '2021-12-10 04:00:00', 'Get learnd', 'Learn how to do a kickflip', 0),
-('TheDude', '2021-12-10 04:20:00', '!KICKFLIP TIME!', 'DO A KICKFLIP!', 0),
-('TheDude', '1998-03-06 00:00:00', 'What the Dude does', 'Abide', 0),
-('Zuccd!', '2021-12-10 04:20:00', '!KICKFLIP TIME!', 'DO A KICKFLIP!', 0),
-('Zuccd!', '2022-01-01 00:00:00', 'Step 4:Profit', 'Sell everyones data', 0),
-('Zuccd!', '2021-12-25 00:00:00', 'Stay low', 'Pretend to be human', 1),
-('Zuccd!', '2100-01-01 00:00:00', 'Endgame', 'Ascend to METAVERSE', 2);
+('areyouTonyHawk@gmail.com', '2021-12-10 04:20:00', '!KICKFLIP TIME!', 'DO A KICKFLIP!', 0),
+('areyouTonyHawk@gmail.com', '2021-12-09 23:59:59', 'Spread the word', 'Tell everyone to DO A KICKFLIP!', 1), 
+('yab0y@gmail.com', '2021-12-10 04:20:00', '!KICKFLIP TIME!', 'DO A KICKFLIP!', 0),
+('yab0y@gmail.com', '2021-12-10 04:00:00', 'Get learnd', 'Learn how to do a kickflip', 0),
+('thedude@gmail.com', '2021-12-10 04:20:00', '!KICKFLIP TIME!', 'DO A KICKFLIP!', 0),
+('thedude@gmail.com', '1998-03-06 00:00:00', 'What the Dude does', 'Abide', 0),
+('givemey0urdata@gmail.com', '2021-12-10 04:20:00', '!KICKFLIP TIME!', 'DO A KICKFLIP!', 0),
+('givemey0urdata@gmail.com', '2022-01-01 00:00:00', 'Step 4:Profit', 'Sell everyones data', 0),
+('givemey0urdata@gmail.com', '2021-12-25 00:00:00', 'Stay low', 'Pretend to be human', 1),
+('givemey0urdata@gmail.com', '2100-01-01 00:00:00', 'Endgame', 'Ascend to METAVERSE', 2);
