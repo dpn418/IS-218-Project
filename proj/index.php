056f15f8fddc951b024527cafcdf674009006889
Jb645 <50148003+Jb645@users.noreply.github.com>
2021-12-10 14:18:33 -0500

    workin version of header (without error)

--- proj/index.php
+++ proj/index.php
@@ -2,12 +2,5 @@
     //checks for URL
 
 //check if sessions are already set
-    if(!isset($_SESSION['errors'])){
-        session_start();
-        if(isset($_SESSION['errors'])){
-            echo"errors are already set";
-        }
-
-    }
     require_once('view/header.php');
 ?>
